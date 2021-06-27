0b9dcd96b4dbe106e7a2a9a68fed784ec961f05d
panjippamungkas <81550517+panjippamungkas@users.noreply.github.com>
2021-06-28 00:07:17 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+// Create database connection using config file
+include_once("config.php");
+ 
+// Fetch all users data from database
+$result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
+?>
+ 
+<html>
+<head>    
+    <title>Homepage</title>
+	 <style>
+         body {font-family:tahoma, arial}
+         table {border-collapse: collapse}
+         th, td {font-size: 13px; border: 1px solid #DEDEDE; padding: 3px 5px; color: #303030}
+         th {background: #CCCCCC; font-size: 12px; border-color:#B0B0B0}
+      </style>
+</head>
+ 
+<body>
+<a href="add.php">Add New User</a><br/><br/>
+ 
+    <table width='60%' border=1>
+ 
+    <tr>
+        <th>Nomer<th>Name</th> <th>Mobile</th> <th>Email</th> <th>Update</th>
+    </tr>
+    <?php  
+    while($user_data = mysqli_fetch_array($result)) {  
+		$no = 1;
+        echo "<tr>";
+		echo "<td>".$no."</td>";
+        echo "<td>".$user_data['name']."</td>";
+        echo "<td>".$user_data['mobile']."</td>";
+        echo "<td>".$user_data['email']."</td>";    
+        echo "<td><a href='edit.php?id=$user_data[id]'>Edit</a> | <a href='delete.php?id=$user_data[id]'>Delete</a></td></tr>";        
+		$no++;
+    }
+    ?>
+    </table>
+</body>
+</html>
